40e98db5513c6b5d6644baf6259c635b119233ce
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-02-15 09:51:19 +0100

    histoire

--- Projet/tree.py
+++ Projet/tree.py
@@ -19,13 +19,20 @@ class Node:
 take_none = Node("Le jeu est pas fini", None, None)
 
 # Histoire si choix = 1
+
+take_mer_poisson = Node(
+    "Vous allez vers la mer et vous trouvez des poissons. Vous décidez de les mettre dans votre inventaire. Mais sur le chemin de votre exploration vous trouvez une carte de l'ile. \n\
+    Que faites-vous ?\n\
+        [1] Vous allez sur le point le plus haut de l'ile pour marquer un message d'alerte.\n\
+        [2] Vous prenez la carte et vous continuez votre exploration.\n\ "
+)
 construire_abri_choice = Node("Vous prenez le matériel et vous vous mettez à construire votre abri de survie. Il vous prend plusieurs heures, mais vous êtes enfin installé et en sécurité. Vous décidez alors d'explorer les alentours pour trouver des ressources.\n\
     Où allez-vous ?\n\
         [1] Vous allez vers la mer pour trouver des poissons.\n\
-        [2] Vous allez vers la forêt.\n\ "
+        [2] Vous allez vers la forêt pour explorer l'ile mais vous oubliez votre sac.\n\ "
 )
-construire_abri_choice.setChoice(1, take_none)
-construire_abri_choice.setChoice(2, take_none)
+construire_abri_choice.setChoice(1, take_mer_poisson)
+construire_abri_choice.setChoice(2, take_take_foret)
 
 recherche_choice= Node(
     "Vous partez à la recherche de ressources. Après avoir marché un moment, vous tombez sur un cours d'eau et décidez de vous installer pour la nuit. La nuit se passe sans encombre et le lendemain, vous reprenez votre exploration. En chemin, vous trouvez une baie remplie de coquillages et décidez de vous arrêter pour en récolter."
@@ -46,6 +53,10 @@ left_choice.setChoice(1, recherche_choice)
 left_choice.setChoice(2, take_none)
 
 # Histoire si choix = 2
+
+take_take_foret = Node(
+    "Vous allez vers la forêt et vous trouvez des fruits. Vous décidez de les mettre dans votre inventaire. Mais sur le chemin vous tombez sur un ours, mais comme vous avez oublié votre sac vous mourrez \n\" GAME OVER \ "
+    )
 right_choice = Node(
     "Vous prenez le colis sur la plage et vous trouvez du matériel pour faire un abri de survie. \n\
         Que faites-vous ? \n\
